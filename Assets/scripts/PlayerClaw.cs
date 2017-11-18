c19f4cb14e6b3986150cfca5f5e5bfb42b21c62b
MONSTER\Deahgib <louis.bennette@gmail.com>
2017-11-18 19:38:25 +0000

    Inital comit

---
+++ Assets/scripts/PlayerClaw.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClaw : MonoBehaviour {
+
+  private GameObject player;
+  private GameObject claw;
+
+  private bool claw_projected = false;
+
+	// Use this for initialization
+	void Start () {
+    player = GameObject.Find("Player");
+    claw = GameObject.Find("Claw");
+  }
+
+  void OnGUI()
+  {
+    Vector3 p = new Vector3();
+    Camera c = Camera.main;
+    Event e = Event.current;
+    Vector2 mousePos = new Vector2();
+
+    // Get the mouse position from Event.
+    // Note that the y position from Event is inverted.
+    mousePos.x = e.mousePosition.x;
+    mousePos.y = c.pixelHeight - e.mousePosition.y;
+
+    p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, c.nearClipPlane));
+
+    GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+    GUILayout.Label("Screen pixels: " + c.pixelWidth + ":" + c.pixelHeight);
+    GUILayout.Label("Mouse position: " + mousePos);
+    GUILayout.Label("World position: " + p.ToString("F3"));
+    GUILayout.EndArea();
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      // Project Claw.
+      // Or Retract Claw.
+    }
+
+    Vector3 p = new Vector3();
+    Camera c = Camera.main;
+    Vector2 mousePos = Input.mousePosition;
+
+    p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, c.nearClipPlane));
+
+    p.z = 0.0f;
+    //cursor.z = player.transform.position.z;
+
+    //claw.transform.position = p;
+
+    Vector3 direction = p - player.transform.position;
+    direction.Normalize();
+    //claw.transform.position = player.transform.position + direction;
+    claw.transform.SetPositionAndRotation(player.transform.position + direction, Quaternion.LookRotation(direction, Vector3.Cross(direction, new Vector3(0,0,1))));
+  }
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+  public float MAX_TORQUE = 30.0f;
+  public float MAX_ROTARION = 500.0f;
+
+  private float torque;
+
+  private GameObject player;
+  private Rigidbody2D rb;
+  public PhysicsMaterial2D physMat;
+
+  struct Physics2DMaterialDefault
+  {
+    public float _friction;
+    public float _bounciness;
+  }
+  private Physics2DMaterialDefault physMatDefault;
+
+  // Use this for initialization
+  void Start () {
+    torque = MAX_TORQUE;
+
+    player = GameObject.Find("Player");
+    rb = player.GetComponent<Rigidbody2D>();
+
+    physMatDefault._friction = physMat.friction;
+    physMatDefault._bounciness = physMat.bounciness;
+
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    //Debug.Log( rb.angularVelocity );
+    if (Input.GetKey(KeyCode.A))
+    {
+      if (rb.angularVelocity < MAX_ROTARION)
+      {
+        rb.AddTorque(torque);
+      }
+      
+      //rb.AddForce(new Vector2(-1.0f,0.0f) * BASE_SPEED);
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      if (rb.angularVelocity > -MAX_ROTARION)
+      {
+        rb.AddTorque(-torque);
+      }
+      //rb.AddForce(new Vector2(1.0f, 0.0f) * BASE_SPEED);
+    }
+	}
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.name.Equals("Slime"))
+    {
+      Destroy(collision.gameObject);
+      StartCoroutine("TimerWait");
+    }
+  }
+
+      
+
+  private IEnumerator TimerWait()
+  {
+    physMat.friction = 0;
+
+    yield return new WaitForSeconds(10.0f); 
+
+    physMat.friction = physMatDefault._friction;
+    Debug.Log("Slime over!");
+  }
+
+}
